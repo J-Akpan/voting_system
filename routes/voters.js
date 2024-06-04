ff174b5f7f882a540b5cb28b0fcbbbbd91cec74e
unknown <jakpan64@yahoo.com>
2024-06-04 18:28:25 +0100

    joi update

--- routes/voters.js
+++ routes/voters.js
@@ -1,3 +1,4 @@
+const Joi = require("joi")
 const db = require("../config/dbConfig");
 const express = require("express");
 const Voter = require("../models/Voters");
@@ -21,8 +22,13 @@ router.get("/login", (req, res) => {
 
 //login handler
 
-router.post("/login", (req, res) =>
-     Voter.findAll({
+router.post("/login", (req, res) =>{
+ 
+    const {email, passwords} = req.body
+    if(!email || !passwords){
+        res.status(404).send(`invalid email or password `)
+    }else{
+    Voter.findAll({
     where: {
       email: req.body.email,
       passwords: req.body.passwords,
@@ -35,7 +41,9 @@ router.post("/login", (req, res) =>
       res.sendStatus(200);
     })
     .catch((err) => console.log(err))
-);
+}
+})
+;
 
 //voters registration
 router.post("/register", (req, res) => {
@@ -53,30 +61,25 @@ router.post("/register", (req, res) => {
   } = req.body;
 
   // val?idate fields
-  let errors = [];
-  if (!voterId) errors.push({ msg: "Please type in you voter" });
+  
+    const schema = Joi.object({
+        voterId: Joi.string().min(3).required(),
+        firstname: Joi.string().required(),
+        lastname: Joi.string().required(),
+        email: Joi.string().min(3).required().email(),
+        passwords: Joi.string().required(),
+        phone: Joi.string(),
+        address: Joi.string().required(),
+        state: Joi.string().required(),
+        lga: Joi.string().required(),
+        ward: Joi.string().required()
 
-  if (!firstname) errors.push({ msg: "Please type in you firstname" });
-
-  if (!lastname) errors.push({ msg: "Please type in you lastname" });
-
-  if (!email) errors.push({ msg: "Please type in you email" });
-
-  if (!passwords) errors.push({ msg: "Please type in you password" });
-
-  if (!phone) errors.push({ msg: "Please type in you phone" });
-
-  if (!address) errors.push({ msg: "Please type in you address" });
-
-  if (!state) errors.push({ msg: "Please type in you state" });
-
-  if (!lga) errors.push({ msg: "Please type in you LGA" });
-
-  if (!ward) errors.push({ msg: "Please type in you ward" });
-
+    })
+    const result = schema.validate(req.body)
+  
   //errors check
-  if (errors.length > 0) {
-    res.send("some fields are empty");
+  if (result.error) {
+    res.status(404).send(result.error.details[0].message);
   } else {
     Voter.create({
       voterId,
@@ -95,10 +98,21 @@ router.post("/register", (req, res) => {
   }
 });
 
+//route to select voters details comparing the voters ID
+router.post("/vote", (req, res) => {
+    const schema = Joi.object({
+        voterId: Joi.string().min(5).required()
+    })
+    const Validation = schema.validate(req.body)
+    if (Validation.error){
+        res.status(404).send(Validation.error.details[0].message)
+    }
+}); //end of route
+
+
 //voters vote casting
 router.get("/vote", (req, res) => {
     //render voters page
   res.send("Welcome to the voters casting page");
 });
-
 module.exports = router;
